958c1cb3df52f684f32b5b719e916da93f37e7b6
katerinacejas <katerinacejas3@gmail.com>
2025-05-29 21:44:33 -0300

    lab

--- e-commerce/src/main/java/com/clicko/e_commerce/model/Producto.java
+++ e-commerce/src/main/java/com/clicko/e_commerce/model/Producto.java
@@ -1,5 +1,8 @@
 package com.clicko.e_commerce.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,8 +12,26 @@ import lombok.Data;
 public class Producto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
 	private Long id;
+
+	@Column(nullable = false, length = 100)
 	private String nombre;
 	private double precio;
+	private String descripcion;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "productos_categorias",
+        joinColumns = @JoinColumn(name = "producto_id"),
+        inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private List<Categoria> categorias = new ArrayList<>();
+
+	@ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "productos_carritos",
+        joinColumns = @JoinColumn(name = "producto_id"),
+        inverseJoinColumns = @JoinColumn(name = "carrito_id")
+    )
+    private List<Carrito> carritos = new ArrayList<>();
 }
